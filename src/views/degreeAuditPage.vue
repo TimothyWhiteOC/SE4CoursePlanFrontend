737e8e4e3f353ce2d977f529d927df62cfa4e6f2
DietrichWasHere <Dietrich.Versaw@eagles.oc.edu>
2021-10-20 16:09:56 -0500

    Beginnings of Degree Audit
    Degree Audit beginnings

--- src/views/degreeAuditPage.vue
+++ src/views/degreeAuditPage.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <h2>Search for Courses</h2>   
-    <input type = "text" v-model= "search" placeholder="Filter by course name &#x1F50E;&#xFE0E;	"/>
+    <h2>Degree Audit</h2>   
     <br>
 
    <!-- <button v-on:click= "cancel">Cancel</button>-->
@@ -20,9 +19,9 @@ export default {
   components: { CourseDisplay },
   data() {
     return {
-      courses: [],
-      search:''
-
+      studentCourses: [],
+      degreePlan: {},
+      
     };
   },
   created() {
@@ -34,18 +33,59 @@ export default {
         console.log('There was an error:', error.response)
       })
   },
-  computed:{
-    filteredCourses: function(){
-      return this.courses.filter((course) => {
-        return course.name.match(this.search);
-      });
-  },
   methods: {
-        cancel() {
-        this.$router.push({ name: 'menu' });
-      }
+    cancel() {
+      this.$router.push({ name: 'menu' });
+    },
+    // may need to install jspdf, autotable
+    // https://stackoverflow.com/questions/54069884/installing-jspdf-using-npm-command
+    // https://cnpmjs.org/package/jspdf-autotable
+    // or may need to import
+    // i used the tutorial code here because its code works
+    // https://codingshiksha.com/vue/vue-js-pdf-generator-in-vuetify-ui-using-jspdf-and-jspdf-autotable-library-full-tutorial-for-beginners/
+    makePDF() {
+      const columns = [
+        { title: "Title", dataKey: "title" },
+        { title: "Body", dataKey: "body" }
+      ];
+      const doc = new jsPDF({
+        orientation: "portrait",
+        unit: "in",
+        format: "letter"
+      });
+      // text is placed using x, y coordinates
+      doc.setFontSize(16).text(this.heading, 0.5, 1.0);
+      // create a line under heading 
+      doc.setLineWidth(0.01).line(0.5, 1.1, 8.0, 1.1);
+      // Using autoTable plugin
+      doc.autoTable({
+        columns,
+        body: this.items,
+        margin: { left: 0.5, top: 1.25 }
+      });
+      // Using array of sentences
+      doc
+        .setFont("helvetica")
+        .setFontSize(12)
+        .text(this.moreText, 0.5, 3.5, { align: "left", maxWidth: "7.5" });
+      
+      // Creating footer and saving file
+      doc
+        .setFont("times")
+        .setFontSize(11)
+        .setFontStyle("italic")
+        .setTextColor(0, 0, 255)
+        .text(
+          "This is a simple footer located .5 inches from page bottom",
+          0.5,
+          doc.internal.pageSize.height - 0.5
+        )
+        .save(`${this.heading}.pdf`);
     }
   }
-
 }
 </script>
+
+<!--import jspdf and autotable here if imports fail 
+  <script src="jspdf.plugin.autotable.min.js"></script>
+-->
--- src/views/mainmenu.vue
+++ src/views/mainmenu.vue
@@ -2,10 +2,10 @@
   <div class="addnew">
     <router-link :to="{ name: 'add'}"><button>Add New Course</button></router-link>
 
-     <router-link :to="{ name: 'listPage'}"><button>Search for Course</button></router-link>
+    <router-link :to="{ name: 'listPage'}"><button>Search for Course</button></router-link>
 
-     <router-link :to="{ name: 'listStudent'}"><button>Search for Students</button></router-link>
+    <router-link :to="{ name: 'listStudent'}"><button>Search for Students</button></router-link>
 
-
+    
   </div>
 </template>
