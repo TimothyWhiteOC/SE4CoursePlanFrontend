f22d773a77c14d001073cd47048ce52f3d9669a0
Timothy White <timothy.white@eagles.oc.edu>
2021-10-27 17:17:15 -0500

    Standardize service files

---
+++ src/services/config.js
@@ -0,0 +1,30 @@
+var baseurl = "";
+if (process.env.NODE_ENV === "development") {
+  baseurl = "http://localhost:8081";
+} else {
+  baseurl = "/api/";
+}
+
+export default {
+  baseURL: baseurl,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "X-Requested-With": "XMLHttpRequest",
+    crossDomain: true
+  },
+  transformRequest: (data, headers) => {
+    let token = localStorage.getItem("token");
+    let authHeader = "";
+    if (token != null && token != "") authHeader = "Bearer " + token;
+    headers.common["Authorization"] = authHeader;
+    return JSON.stringify(data);
+  },
+  transformResponse: function(data) {
+    data = JSON.parse(data);
+    if (!data.success && data.code == "expired-session") {
+      localStorage.deleteItem("token");
+    }
+    return data;
+  }
+}
